77b65051371e0ca9fc65cb11ebd4ddb0ecfc1e26
IGOR COJOCARU <cjc.igor@gmail.com>
2019-05-24 15:58:58 +0200

    initial

---
+++ build/precache-manifest.cbbb70eb6ab651190755e137a1bcd52c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc464f1c06dc0258a1892249248bcc42",
+    "url": "https//ygryc.github.io/contact_manager/index.html"
+  },
+  {
+    "revision": "57bb75182fbee9d9bce8",
+    "url": "https//ygryc.github.io/contact_manager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "e5b5cbaaaa4f82da2041",
+    "url": "https//ygryc.github.io/contact_manager/static/css/main.65938976.chunk.css"
+  },
+  {
+    "revision": "57bb75182fbee9d9bce8",
+    "url": "https//ygryc.github.io/contact_manager/static/js/2.02a1030e.chunk.js"
+  },
+  {
+    "revision": "e5b5cbaaaa4f82da2041",
+    "url": "https//ygryc.github.io/contact_manager/static/js/main.e62ca83f.chunk.js"
+  },
+  {
+    "revision": "9c0d66ee0b5c6f90a642",
+    "url": "https//ygryc.github.io/contact_manager/static/js/runtime~main.92dfc283.js"
+  }
+]);
---
+++ src/components/contacts/AddContact.js
@@ -0,0 +1,130 @@
+import React, { Component } from 'react';
+import { Consumer } from '../../context';
+import TextInputGroup from '../layout/TextInputGroup';
+import axios from 'axios';
+import { async } from 'q';
+class AddContact extends Component {
+  state = {
+    name: '',
+    email: '',
+    phone: '',
+    errors: {}
+  };
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  onSubmit = async (dispatch, e) => {
+    e.preventDefault();
+
+    const { name, email, phone } = this.state;
+
+    //Check for Errors
+    if (name === '') {
+      this.setState({
+        errors: {
+          name: 'Name is Required'
+        }
+      });
+      return;
+    }
+
+    if (email === '') {
+      this.setState({
+        errors: {
+          email: 'Email is Required'
+        }
+      });
+      return;
+    }
+
+    if (phone === '') {
+      this.setState({
+        errors: {
+          phone: 'Phone is Required'
+        }
+      });
+      return;
+    }
+
+    const newContact = {
+      name,
+      email,
+      phone
+    };
+
+    const res = await axios.post(
+      'https://jsonplaceholder.typicode.com/users',
+      newContact
+    );
+
+    dispatch({ type: 'ADD_CONTACT', payload: res.data });
+
+    /*axios
+      .post('https://jsonplaceholder.typicode.com/users', newContact)
+      .then(res => dispatch({ type: 'ADD_CONTACT', payload: res.data }));*/
+
+    //Clear state after submiting the form
+    this.setState({
+      name: '',
+      email: '',
+      phone: '',
+      errors: {}
+    });
+    this.props.history.push('/');
+  };
+
+  render() {
+    const { name, email, phone, errors } = this.state;
+
+    return (
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div className='card mb-3'>
+              <div className='card-header'>Add Contact</div>
+              <div className='card-body'>
+                <form onSubmit={this.onSubmit.bind(this, dispatch)}>
+                  <TextInputGroup
+                    label='Name'
+                    name='name'
+                    placeholder='Enter name...'
+                    value={name}
+                    onChange={this.onChange}
+                    error={errors.name}
+                  />
+
+                  <TextInputGroup
+                    label='Email'
+                    name='email'
+                    type='email'
+                    placeholder='Enter email...'
+                    value={email}
+                    onChange={this.onChange}
+                    error={errors.email}
+                  />
+
+                  <TextInputGroup
+                    label='Phone'
+                    name='phone'
+                    placeholder='Enter phone...'
+                    value={phone}
+                    onChange={this.onChange}
+                    error={errors.phone}
+                  />
+
+                  <input
+                    type='submit'
+                    value='Add Contact'
+                    className='btn btn-danger btn-block'
+                  />
+                </form>
+              </div>
+            </div>
+          );
+        }}
+      </Consumer>
+    );
+  }
+}
+export default AddContact;
