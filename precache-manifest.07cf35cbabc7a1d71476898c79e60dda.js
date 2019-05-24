20f2258c07c0749d5e2e16439fb27bb59e7d6021
IGOR COJOCARU <cjc.igor@gmail.com>
2019-05-24 16:13:32 +0200

    Updates

---
+++ precache-manifest.07cf35cbabc7a1d71476898c79e60dda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5ce4e88424bdaf4858ade1fa674b8c6",
+    "url": "/contact_manager/index.html"
+  },
+  {
+    "revision": "05ba37b63984012c68ae",
+    "url": "/contact_manager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "9573d044e0eb201a3718",
+    "url": "/contact_manager/static/css/main.65938976.chunk.css"
+  },
+  {
+    "revision": "05ba37b63984012c68ae",
+    "url": "/contact_manager/static/js/2.85606205.chunk.js"
+  },
+  {
+    "revision": "9573d044e0eb201a3718",
+    "url": "/contact_manager/static/js/main.8d116593.chunk.js"
+  },
+  {
+    "revision": "2b941c4b3b0d1aff9d5e",
+    "url": "/contact_manager/static/js/runtime~main.75798bfb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/contactmanager/precache-manifest.615fb90041f523c8b668e7abc7137ca8.js"
+  "/contact_manager/precache-manifest.07cf35cbabc7a1d71476898c79e60dda.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/contactmanager/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/contact_manager/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
